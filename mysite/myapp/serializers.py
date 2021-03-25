bf6567f5657c55376008e8c7e5e3c8fa333ca34e
parves <parves5405@gmail.com>
2021-03-25 18:03:16 +0600

    serializer added

---
+++ mysite/myapp/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Trainings
+
+
+class TrainingSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Trainings
+        fields = ['id', 'training_name', 'certificate']
